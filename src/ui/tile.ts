0bc9718061143757eb26ad0be01aeb9d1ba19665
Sandbar Polymer <sandbar0polymer@icloud.com>
2022-06-25 09:48:34 +0200

    Update tile display and set custom data

--- src/ui/tile.ts
+++ src/ui/tile.ts
@@ -1,4 +1,5 @@
 import { ColorMaterialProperty, ConstantProperty, Viewer } from "cesium";
+import { utils } from "ethers";
 import { EARTH, LAND } from "../contract/type";
 import { DEFAULT_SURFACE_COLOR, OUTLINE_COLOR, OUTLINE_COLOR_SELECTED, TileEntity } from "../grid";
 import { closeAllModals, handlePromiseRejection, ZERO_ADDRESS } from "../util";
@@ -45,10 +46,45 @@ export function initTileModal(viewer: Viewer, tiles: TileEntity[], earth: EARTH,
     document.getElementById('tile-modal-index').innerHTML = `${index.toString()}`;
     document.getElementById('tile-modal-coordinates').innerHTML = formatCoordinates(tiles[index].coordinates);
     document.getElementById('tile-modal-owner').innerHTML = hasOwner ? owner.toString() : 'None';
+    document.getElementById('tile-modal-buy').style.display = hasOwner ? 'none' : 'initial';
 
-    async function updateButton() {
+    async function updateCustomData() {
+      document.getElementById('tile-modal-customdata').style.display = hasOwner ? 'initial' : 'none';
+      const acc = await earth.signer.getAddress();
+      document.getElementById('tile-modal-customdata-setdata').style.display = owner == acc ? 'initial' : 'none';
+      const customData = await earth.customData(index);
+      const customDataDisplay = document.getElementById('tile-modal-customdata-value');
+      customDataDisplay.innerText = utils.toUtf8String(customData);
+      
+      const submit = document.getElementById('tile-modal-customdata-submit') as HTMLButtonElement;
+      submit.onclick = async e => {
+        let loading = document.getElementById("tile-modal-customdata-loader");
+        let loadingText = document.getElementById("tile-modal-customdata-loader-text");
+        submit.disabled = true;
+        loading.style.display = "initial";
+        loadingText.innerText = "Submitting transaction...";
+
+        try {
+          const input = document.getElementById('tile-modal-customdata-input') as HTMLInputElement;
+          const value = input.value;
+          const valueBytes = utils.toUtf8Bytes(value);
+          const tx = await earth.setCustomData(index, valueBytes);
+          loadingText.innerText = "Waiting for transaction confirmation...";
+          await tx.wait();
+          customDataDisplay.innerText = value;
+          input.value = '';
+        } catch (e) {
+          handlePromiseRejection(e);
+        } finally {
+          submit.disabled = false;
+          loading.style.display = "none";
+        }
+      }
+    }
+    await updateCustomData();
+
+    async function updateBuyButton() {
       const buy = document.getElementById('tile-modal-button-buy') as HTMLButtonElement;
-      const tradeInfo = document.getElementById('tile-modal-trade-info');
       if (!hasOwner) {
         // Update on click handler.
         buy.onclick = async e => {
@@ -93,16 +129,14 @@ export function initTileModal(viewer: Viewer, tiles: TileEntity[], earth: EARTH,
           buy.disabled = false;
           getLND.style.display = 'none';
         }
-        tradeInfo.style.display = 'none';
       } else {
         // Disable buy button.
         buy.disabled = true;
         getLND.style.display = 'none';
-        tradeInfo.style.display = 'initial';
       }
     }
 
-    await updateButton();
+    await updateBuyButton();
 
     // Show modal.
     modal.style.display = "block";
